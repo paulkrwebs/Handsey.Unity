f8eada706214c946c74acfb29ff8d35dda681546
Paul Kiernan <paulk@rwebs.co.uk>
2016-02-14 18:18:17 +0000

    adding full code

---
+++ Handsey.Unity.Tests/UnityIocContainerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Practices.Unity;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handsey.Unity.Tests
+{
+    [TestFixture]
+    public class UnityIocContainerTests
+    {
+        private Mock<IUnityContainer> _unityContainer;
+        private UnityIocContainer _unityIocContainer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _unityContainer = new Mock<IUnityContainer>();
+            _unityIocContainer = new UnityIocContainer(_unityContainer.Object);
+        }
+
+        [Test]
+        public void Register_TypeAndType_TypeRegsiteredOnUnityContainer()
+        {
+            _unityIocContainer.Register(typeof(int), typeof(string));
+            // Can't mock the extension method so mocking the method the extension method calls (not ideal!)
+            _unityContainer.Verify(u => u.RegisterType(typeof(int), typeof(string), It.IsAny<string>(), It.IsAny<LifetimeManager>()), Times.Once(), "Regsiter was not called on the unity container");
+        }
+
+        [Test]
+        public void Register_TypeAndTypeAndString_TypeRegsiteredOnUnityContainerWithName()
+        {
+            _unityIocContainer.Register(typeof(int), typeof(string), "dave");
+            // Can't mock the extension method so mocking the method the extension method calls (not ideal!)
+            _unityContainer.Verify(u => u.RegisterType(typeof(int), typeof(string), It.Is<string>(x => x == "dave"), It.IsAny<LifetimeManager>()), Times.Once(), "Regsiter was not called on the unity container");
+        }
+
+        [Test]
+        public void ResolveAll_NoParams_ResolveAllCalledOnUnityContainer()
+        {
+            _unityIocContainer.ResolveAll<int>();
+            // Can't mock the extension method so mocking the method the extension method calls (not ideal!)
+            _unityContainer.Verify(u => u.ResolveAll(It.IsAny<Type>()), Times.Once(), "Resolve all was not called on the unity container");
+        }
+    }
+}
---
+++ Handsey.Unity/UnityIocContainer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handsey.Unity
+{
+    public class UnityIocContainer : IIocContainer
+    {
+        private readonly IUnityContainer _unityContainer;
+
+        public UnityIocContainer(IUnityContainer unityContainer)
+        {
+            _unityContainer = unityContainer;
+        }
+
+        public void Register(Type from, Type to)
+        {
+            _unityContainer.RegisterType(from, to);
+        }
+
+        public void Register(Type from, Type to, string name)
+        {
+            _unityContainer.RegisterType(from, to, name);
+        }
+
+        public TResolve[] ResolveAll<TResolve>()
+        {
+            return _unityContainer.ResolveAll<TResolve>().ToArray();
+        }
+    }
+}
